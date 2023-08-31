562079fea89842cf123f90fb5933f31c6e233b22
Mohd Hasan <125153993+HasanN-tech@users.noreply.github.com>
2023-08-31 19:38:42 +0930

    Update Practise.java

--- com/tutorial/Practise.java
+++ com/tutorial/Practise.java
@@ -139,3 +139,22 @@ public class Practise {
         
     }
 }
+class MyComparator implements Comparator
+{
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        Integer I1=(Integer)o1;
+        Integer I2=(Integer)o2;
+//        if (I1<I2)
+//            return +1;
+//        else if (I1 > I2)
+//            return -1;
+//        else
+//            return 0;
+        return I2.compareTo(I1);
+       // return -I1.compareTo(I2);
+       // return +1;
+        //  return 0;
+    }
+}
